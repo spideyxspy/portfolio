849e4353fff0c8c1c44a8547e8c94fd007cb01ef
murphyie <murphyiee@outlook.com>
2026-03-09 15:50:03 +0530

    Update skills.tsx

--- src/components/sections/skills.tsx
+++ src/components/sections/skills.tsx
@@ -4,7 +4,7 @@ import { SectionHeader } from "./section-header";
 const SkillsSection = () => {
   return (
     <SectionWrapper id="skills" className="w-full h-screen md:h-[150dvh] pointer-events-none">
-      <SectionHeader id='skills' title="Tech Stack" desc="(hint: press a key)" />
+      <SectionHeader id='skills' title="Editor/Designer" desc="(hint: press a key)" />
     </SectionWrapper>
   );
 };
