38708ab1c834ea4fb2bf5ac049ccaa33e99b1b86
murphyie <murphyiee@outlook.com>
2026-03-10 13:58:22 +0530

    Update projects.tsx

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -7,17 +7,17 @@ export type Project = {
 const projects: Project[] = [
   {
     title: "3D Portfolio",
-    src: "/assets/projects-screenshots/project1.jpg",
+    src: "/assets/projects-screenshots/project1.jpeg",
     type: "image",
   },
   {
     title: "Landing Page Design",
-    src: "/assets/projects-screenshots/project2.jpg",
+    src: "/assets/projects-screenshots/project2.jpeg",
     type: "image",
   },
   {
     title: "Dashboard UI",
-    src: "https://substackcdn.com/image/fetch/$s_!G1lk!,w_1456,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F8ed3d547-94ff-48e1-9f20-8c14a7030a02_2000x2000.jpeg",
+    src: "/assets/projects-screenshots/project3.jpeg",
     type: "image",
   },
   {
