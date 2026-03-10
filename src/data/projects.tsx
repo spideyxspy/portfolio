4cdc9a630824e5248a134abd626b901da5fb4d5d
murphyie <murphyiee@outlook.com>
2026-03-10 13:51:01 +0530

    Update projects.tsx

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -17,7 +17,7 @@ const projects: Project[] = [
   },
   {
     title: "Dashboard UI",
-    src: "/assets/projects-screenshots/project3.jpg",
+    src: "https://substackcdn.com/image/fetch/$s_!G1lk!,w_1456,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F8ed3d547-94ff-48e1-9f20-8c14a7030a02_2000x2000.jpeg",
     type: "image",
   },
   {
