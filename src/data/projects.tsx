fdc13db894d8dd324dae36a452e97806163e26ce
murphyie <murphyiee@outlook.com>
2026-03-10 13:35:22 +0530

    Update projects.tsx

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,4 +1,8 @@
-import { ReactNode } from "react";
+import project1 from "./project1.jpg";
+import project2 from "./project2.jpg";
+import project3 from "./project3.jpg";
+import project4 from "./project4.jpg";
+import video1 from "./video1.mp4";
 
 export type Project = {
   title: string;
@@ -9,27 +13,27 @@ export type Project = {
 const projects: Project[] = [
   {
     title: "3D Portfolio",
-    src: "./project1.jpg",
+    src: project1,
     type: "image",
   },
   {
     title: "Landing Page Design",
-    src: "./project2.jpg",
+    src: project2,
     type: "image",
   },
   {
     title: "Dashboard UI",
-    src: "./project3.jpg",
+    src: project3,
     type: "image",
   },
   {
     title: "Product Animation",
-    src: "./project4.jpg",
+    src: project4,
     type: "image",
   },
   {
     title: "Website Interaction Demo",
-    src: "./video1.mp4",
+    src: video1,
     type: "video",
   },
 ];
