fd23a972e0a2e30f200a5ad7656dd985727ee40f
murphyie <murphyiee@outlook.com>
2026-03-10 13:27:49 +0530

    Update projects.tsx

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -9,34 +9,29 @@ export type Project = {
 const projects: Project[] = [
   {
     title: "3D Portfolio",
-    src: "/assets/projects-screenshots/project1.jpg",
+    src: "./project1.jpg",
     type: "image",
   },
   {
     title: "Landing Page Design",
-    src: "/assets/projects-screenshots/project2.jpg",
+    src: "./project2.jpg",
     type: "image",
   },
   {
     title: "Dashboard UI",
-    src: "/assets/projects-screenshots/project3.jpg",
+    src: "./project3.jpg",
     type: "image",
   },
   {
     title: "Product Animation",
-    src: "/assets/projects-screenshots/project4.jpg",
+    src: "./project4.jpg",
     type: "image",
   },
   {
     title: "Website Interaction Demo",
-    src: "/assets/projects-screenshots/video1.mp4",
+    src: "./video1.mp4",
     type: "video",
   },
-  // {
-  //   title: "App Preview",
-  //   src: "/assets/projects-screenshots/video3.mp4",
-  //   type: "video",
-  // },
 ];
 
 export default projects;
